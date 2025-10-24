bf1105fe58aa98c6e85ac7e3d303c53a64834dc6
LatisAhn <denny.ahn@textree.co.kr>
2025-10-24 23:28:11 +0900

    Move AI Recommended Camps section further down (100px spacing)

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "53cb2af9a46ebc04328589b21ef0e433",
+const RESOURCES = {"flutter_bootstrap.js": "cff98f5dd4f3f14b3ad98e53f16a1505",
 "version.json": "f9e55108067bbee6898c8de8534b436f",
 "index.html": "92acbdf80dd429c3411e6bbb6b9ffd3b",
 "/": "92acbdf80dd429c3411e6bbb6b9ffd3b",
-"main.dart.js": "407098f3ab6b49d678f9be8731643030",
+"main.dart.js": "9213c7c2577f16e2bb14e1e1bfd91ca1",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
