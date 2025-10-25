d5ba0f763b5b912aa29402bf55ee66f3f990659c
LatisAhn <denny.ahn@textree.co.kr>
2025-10-25 09:17:50 +0900

    Rebuild and redeploy community page

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d2913632a4578ee4d0b8b1c4a69888c8a0672c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "114239301"
+    serviceWorkerVersion: "344275871"
   }
 });
